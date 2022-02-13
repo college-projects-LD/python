f87188414d1d8c8d74c1e4f568a3d9e05caf696f
collage-projects-LD <delaneyboys16@gmail.com>
2022-02-12 18:27:32 -0700

    n

--- programming with functions/sentences.py
+++ programming with functions/sentences.py
@@ -101,17 +101,50 @@ def get_verb(quantity, tense):
     verb = random.choice(verbs)
     return verb
 
+def get_preposition():
+    """Return a randomly chosen preposition
+    from this list of prepositions:
+        "about", "above", "across", "after", "along",
+        "around", "at", "before", "behind", "below",
+        "beyond", "by", "despite", "except", "for",
+        "from", "in", "into", "near", "of",
+        "off", "on", "onto", "out", "over",
+        "past", "to", "under", "with", "without"
+
+    Return: a randomly chosen preposition.
+    """
+    prepositions = ["about", "above", "across", "after", "along",
+        "around", "at", "before", "behind", "below",
+        "beyond", "by", "despite", "except", "for",
+        "from", "in", "into", "near", "of",
+        "off", "on", "onto", "out", "over",
+        "past", "to", "under", "with", "without"]
+    preposition = random.choice(prepositions)
+    return preposition
+def get_prepositional_phrase(quantity):
+    """Build and return a prepositional phrase composed of three
+    words: a preposition, a determiner, and a noun by calling the
+    get_preposition, get_determiner, and get_noun functions.
+
+    Parameter
+        quantity: an integer that determines if the determiner
+            and noun in the prepositional phrase returned from
+            this function are single or pluaral.
+    Return: a prepositional phrase.
+    """
+    phrase = get_preposition()+ " " +get_determiner(quantity)  +  " " + get_noun(quantity)
+    return phrase
 
 
 def main():
 
 
-    print(f'{get_determiner(1).capitalize()} {get_noun(1)} {get_verb(1,"past")}')
-    print(f'{get_determiner(1).capitalize()} {get_noun(1)} {get_verb(1,"present")}')
-    print(f'{get_determiner(1).capitalize()} {get_noun(1)} {get_verb(1,"future")}')
-    print(f'{get_determiner(2).capitalize()} {get_noun(2)} {get_verb(2,"past")}')
-    print(f'{get_determiner(2).capitalize()} {get_noun(2)} {get_verb(2,"present")}')
-    print(f'{get_determiner(2).capitalize()} {get_noun(2)} {get_verb(2,"future")}')
+    print(f'{get_determiner(1).capitalize()} {get_noun(1)} {get_verb(1,"past")} {get_prepositional_phrase(1)}')
+    print(f'{get_determiner(1).capitalize()} {get_noun(1)} {get_verb(1,"present")} {get_prepositional_phrase(1)}')
+    print(f'{get_determiner(1).capitalize()} {get_noun(1)} {get_verb(1,"future")} {get_prepositional_phrase(1)}')
+    print(f'{get_determiner(2).capitalize()} {get_noun(2)} {get_verb(2,"past")} {get_prepositional_phrase(2)}')
+    print(f'{get_determiner(2).capitalize()} {get_noun(2)} {get_verb(2,"present")} {get_prepositional_phrase(2)}')
+    print(f'{get_determiner(2).capitalize()} {get_noun(2)} {get_verb(2,"future")} {get_prepositional_phrase(2)}')
 
 
 main()
